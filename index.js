8a6df2bb8e57f83130706395d85d5665de14e8b1
Naphattharawat S <naphattharawat@gmail.com>
2023-02-02 13:31:41 +0700

    update

--- index.js
+++ index.js
@@ -38,9 +38,9 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './dist/index.html'));
 
 });
-app.get('/2', (req, res) => {
+app.get('/download', (req, res) => {
   // app.use(express.static(path.join(__dirname, 'dist')));
-  res.sendFile(path.join(__dirname, './indexold.html'));
+  res.sendFile(path.join(__dirname, './download.html'));
 
 });
 app.get('/3', (req, res) => {
