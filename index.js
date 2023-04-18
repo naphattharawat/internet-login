7f49f3ac10135b5c3cacc20e7771152cef061645
Naphattharawat S <naphattharawat@gmail.com>
2023-04-18 12:34:01 +0700

    update

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ app.use(bodyParser.json({ limit: '5mb' }));
 app.use(bodyParser.urlencoded({ extended: false }));
 const io = new Server(server, {
   secure: true,
-  cors: {
-    origin: "dev.moph.go.th",
-    methods: ["GET", "POST"],
-    credentials: true
-  }
+  // cors: {
+  //   origin: "dev.moph.go.th",
+  //   methods: ["GET", "POST"],
+  //   credentials: true
+  // }
 });
 // const io = new Server(server,
 //   {
